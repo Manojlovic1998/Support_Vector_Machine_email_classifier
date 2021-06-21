b11bfebb3afa45f8b9d19e480facb28a89f0094a
Manojlovic1998 <manojlovic911@hotmail.com>
2021-06-21 05:06:15 +0200

    Change parameters for classifier

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 from time import time
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 from tools.email_preprocess import preprocess
 
 words_file = "/word_data.pkl"
@@ -7,10 +8,10 @@ authors_file = "/email_authors.pkl"
 
 features_train, features_test, labels_train, labels_test = preprocess(words_file, authors_file)
 
-features_train = features_train[:int(len(features_train)/100)]
-labels_train = labels_train[:int(len(labels_train)/100)]
 
-clf = SVC(kernel='rbf')
+
+# NOTE: The default value of gamma changed from "auto" to "scale"
+clf = SVC(kernel='rbf', C=10000, gamma='auto')
 
 print("Started Training the model!")
 time_fit = time()
@@ -20,8 +21,9 @@ time_fit_end = time()
 print(f"Training time was: {time_fit_end - time_fit}")
 
 time_predict = time()
-clf.predict(features_test)
+pred = clf.predict(features_test)
 time_predict_end = time()
+
 print(f"Predicting time was: {time_predict_end - time_predict}")
 
-print(f"Model score: {clf.score(features_test, labels_test)}")
+print(f"Model score: {accuracy_score(pred, labels_test)}")
